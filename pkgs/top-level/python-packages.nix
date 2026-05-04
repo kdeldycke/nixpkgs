b6e031db815ec7567863333edb73329f9167239e
Kevin Deldycke <kevin@deldycke.com>
2026-05-04 20:26:42 +0200

    python3Packages.extra-platforms: init at 12.0.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5284,6 +5284,8 @@ self: super: with self; {
 
   extension-helpers = callPackage ../development/python-modules/extension-helpers { };
 
+  extra-platforms = callPackage ../development/python-modules/extra-platforms { };
+
   extra-streamlit-components =
     callPackage ../development/python-modules/extra-streamlit-components
       { };

8869f601742ad92a32cc87b175dbc38cf59de7c7
Kevin Deldycke <kevin@deldycke.com>
2026-05-04 20:26:21 +0200

    python3Packages.click-extra: init at 7.15.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -2841,6 +2841,8 @@ self: super: with self; {
 
   click-didyoumean = callPackage ../development/python-modules/click-didyoumean { };
 
+  click-extra = callPackage ../development/python-modules/click-extra { };
+
   click-help-colors = callPackage ../development/python-modules/click-help-colors { };
 
   click-log = callPackage ../development/python-modules/click-log { };
